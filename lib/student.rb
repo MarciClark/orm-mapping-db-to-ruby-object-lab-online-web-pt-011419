c5baeeabeb7d4ba9b0491257a4ed1dc27bf7286a
Marci Clark <marcig612@gmail.com>
2019-04-01 19:51:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -104,7 +104,7 @@ class Student
     end.first
   end
   
-def self.all_students_in_grade_x(grade)
+  def self.all_students_in_grade_x(grade)
     sql = <<-SQL
       SELECT * 
       FROM students
