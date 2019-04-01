082613a9e9e3ca8a1bd5bcaaaf3780f2813ccde7
Marci Clark <marcig612@gmail.com>
2019-04-01 19:46:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,8 @@ class Student
 
   def self.all
     sql = <<-SQL
-      SELECT * FROM students
+      SELECT * 
+      FROM students
       SQL
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
@@ -20,7 +21,8 @@ class Student
 
   def self.find_by_name(name)
     sql = <<-SQL
-    SELECT * FROM students
+    SELECT * 
+    FROM students
     WHERE name = ?
     LIMIT 1
     SQL
@@ -57,7 +59,8 @@ class Student
   
   def self.all_students_in_grade_9
     sql = <<-SQL
-      SELECT * FROM students
+      SELECT * 
+      FROM students
       WHERE students.grade = 9
     SQL
     
